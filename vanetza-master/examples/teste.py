4b11e427da77ebf4b280ca131e95878e3260e7b4
Filipe Vale <fmmvale@hotmail.com>
2024-05-30 11:43:17 +0100

    Generate with compose

--- vanetza-master/examples/teste.py
+++ vanetza-master/examples/teste.py
@@ -1,10 +1,54 @@
-
+import json
 import geopy.distance
 import math
 
-#https://gist.github.com/jeromer/2005586
+simple_positions = [[(40.64196130815548, -8.651599817893402), (40.64260232822538, -8.648281407423386)],
+                    [(40.6425641904421, -8.648104086128557), (40.641601012057706, -8.647785673242867)],
+                    [(40.64158087763361, -8.647778626816859), (40.642531400552464, -8.648082975328139)],
+                    [(40.642716577699275, -8.64798879195188), (40.6429279060416, -8.646700223922215)],
+                    [(40.642882822718036, -8.6465925337987),(40.64244607644473, -8.646451422602368)],
+                    [(40.64244607644473, -8.646451422602368),(40.642882822718036, -8.6465925337987)],
+                    [(40.642953566928895, -8.646489987939923),(40.64397914538013, -8.641272445716009)],
+                    [(40.644105963126066, -8.6414250479259),(40.643107956090425, -8.646453654080425)],
+                    [(40.64314103943572, -8.646678924009313),(40.64394606245026, -8.6469550613415)],
+                    [(40.64394606245026, -8.6469550613415),(40.64314103943572, -8.646678924009313)],
+                    [(40.643014219856376, -8.646715257868811),(40.642777121735605, -8.647972409407442)],
+                    [(40.642854316565064, -8.64820494610823),(40.64360971839902, -8.648502883756114)],
+                    [(40.64360971839902, -8.648502883756114),(40.642854316565064, -8.64820494610823)],
+                    [(40.64274403820994, -8.648321214458623),(40.642060308335076, -8.65155492832519)],
+                    ]
+
+current_pos = (0,0)
+final_pos = (0,0)
+speed = 60 / 3.6  # km /3.6 = m/s
+TIME = 0.5  # seconds
+current_bearing = 0
+index = 1
+first = True
+distance = 0.005
+position_index = 0;
+def next_position(current_p, final_p, tempo):
+    """
+    Calcula a próxima posição com base em coordenadas GPS e velocidade.
+
+    Args:
+      coordenadas_gps: Tupla com latitude e longitude (em graus).
+      velocidade_x: Velocidade na direção x (em metros por segundo).
+      velocidade_y: Velocidade na direção y (em metros por segundo).
+      tempo: Tempo em segundos.
+
+    Returns:
+      Tupla com as coordenadas da próxima posição (latitude e longitude em graus).
+    """
+    global current_bearing, index, TIME, simple_positions, final_pos, distance
+    bearing = calculate_initial_compass_bearing(current_p, final_p)
+    end_point = geopy.distance.distance(kilometers=distance).destination(current_pos, bearing)
+    current_bearing = bearing
+    return end_point[0], end_point[1]
+
+
 def calculate_initial_compass_bearing(pointA, pointB):
-  """
+    """
   Calculates the bearing between two points.
   The formulae used is the following:
       θ = atan2(sin(Δlong).cos(lat2),
@@ -19,36 +63,43 @@ def calculate_initial_compass_bearing(pointA, pointB):
   :Returns Type:
     float
   """
-  if (type(pointA) != tuple) or (type(pointB) != tuple):
-    raise TypeError("Only tuples are supported as arguments")
+    if (type(pointA) != tuple) or (type(pointB) != tuple):
+        raise TypeError("Only tuples are supported as arguments")
 
-  lat1 = math.radians(pointA[0])
-  lat2 = math.radians(pointB[0])
+    lat1 = math.radians(pointA[0])
+    lat2 = math.radians(pointB[0])
 
-  diffLong = math.radians(pointB[1] - pointA[1])
+    diffLong = math.radians(pointB[1] - pointA[1])
 
-  x = math.sin(diffLong) * math.cos(lat2)
-  y = math.cos(lat1) * math.sin(lat2) - (math.sin(lat1)
-                                         * math.cos(lat2) * math.cos(diffLong))
+    x = math.sin(diffLong) * math.cos(lat2)
+    y = math.cos(lat1) * math.sin(lat2) - (math.sin(lat1)
+                                           * math.cos(lat2) * math.cos(diffLong))
 
-  initial_bearing = math.atan2(x, y)
+    initial_bearing = math.atan2(x, y)
 
-  # Now we have the initial bearing but math.atan2 return values
-  # from -180° to + 180° which is not what we want for a compass bearing
-  # The solution is to normalize the initial bearing as shown below
-  initial_bearing = math.degrees(initial_bearing)
-  compass_bearing = (initial_bearing + 360) % 360
+    # Now we have the initial bearing but math.atan2 return values
+    # from -180° to + 180° which is not what we want for a compass bearing
+    # The solution is to normalize the initial bearing as shown below
+    initial_bearing = math.degrees(initial_bearing)
+    compass_bearing = (initial_bearing + 360) % 360
 
-  return compass_bearing
-
-p1 = (40.64196130815548, -8.651599817893402)
-p2 = (40.64260232822538, -8.648281407423386)
-speed = 10/3.6  #km /3.6 = m/s
-Time = 0.5 #seconds
-distance = speed * Time/1000
-bearing = calculate_initial_compass_bearing(p1,p2)
+    return compass_bearing
 
 
-end_point = geopy.distance.distance(kilometers=distance).destination(p1, bearing)
+print("\n\n")
+roads = []
+for p in range(len(simple_positions)):
+    a = [simple_positions[p][0]]
 
-print(end_point.latitude, end_point.longitude)
+    N_Samples = int(geopy.distance.geodesic(simple_positions[p][0], simple_positions[p][1]).km / distance)
+    current_pos = simple_positions[p][0]
+    final_pos = simple_positions[p][1]
+    for i in range(N_Samples):
+      next_latitude, next_longitude = next_position(current_pos, final_pos, TIME)
+      current_pos = (next_latitude, next_longitude)
+      a.append(current_pos)
+    a.append(simple_positions[p][1])
+    roads.append(a)
+    # print("list_"+str(p)+" = "+str(a))
+print("roads = "+str(roads))
+print("\n\n")
--- webapp/src/MapComponent.js
+++ webapp/src/MapComponent.js
@@ -1,49 +1,58 @@
 import React, { useState, useEffect } from 'react';
-import { MapContainer, TileLayer, Marker, Popup} from 'react-leaflet';
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
 import './MapComponent.css';
 import L from 'leaflet';
-import { ToastContainer, toast } from 'react-toastify';
+import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import Toggle from 'react-toggle';
-import "react-toggle/style.css";
-
-// const CustomToast = ({ content, description }) => (
-//   <div>
-//     <div>{content}</div>
-//     <h5>{description}</h5>
-//   </div>
-// );
 
 function MapComponent() {
-  const [obuData, setObuData] = useState([]);
-  const [rsuData, setRsuData] = useState([]);
-  // const [showHighwayInfo, setShowHighwayInfo] = useState(true);
+  const [carData, setCarData] = useState([]);
+
+  // Static traffic light positions
+  const trafficLights = [
+    { id: 'tl1', latitude: 40.642808, longitude: -8.648206 },
+    { id: 'tl2', latitude: 40.642643, longitude: -8.648292 },
+    { id: 'tl3', latitude: 40.642554, longitude: -8.648119 },
+    { id: 'tl4', latitude: 40.642714, longitude: -8.647976 },
+    { id: 'tl5', latitude: 40.642955, longitude: -8.646786 },
+    { id: 'tl6', latitude: 40.642865, longitude: -8.646602 },
+    { id: 'tl7', latitude: 40.643022, longitude: -8.646451 },
+    { id: 'tl8', latitude: 40.643103, longitude: -8.646651 }
+  ];
+
+  // Static numbers positions
+  const numberPositions = [
+    { id: 'n1', position: [40.64246735129016, -8.6485201216305], number: '0' },
+    { id: 'n1', position: [40.642399899557034, -8.648201329699718], number: '1' },
+    { id: 'n1', position: [40.64244641800096, -8.647897864304072], number: '2' },
+    { id: 'n1', position: [40.64264877285484, -8.647677162198146], number: '3' },
+    { id: 'n1', position: [40.64280693484221, -8.646812745616604], number: '3' },
+    { id: 'n1', position: [40.64268366156104, -8.646717720913703], number: '4' },
+    { id: 'n1', position: [40.64273948345254, -8.646346818763465], number: '5' },
+    { id: 'n1', position: [40.64295114103379, -8.646009634990524], number: '6' },
+    { id: 'n1', position: [40.64326400557164, -8.646167127935588], number: '7' },
+    { id: 'n1', position: [40.643261384867486, -8.646536688850805], number: '8' },
+    { id: 'n1', position: [40.64316179803358, -8.64696151121129], number: '9' },
+    { id: 'n1', position: [40.64307269389833, -8.647044403379189], number: '10' },
+    { id: 'n1', position: [40.64289710598954, -8.64789404810016], number: '10' },
+    { id: 'n1', position: [40.64298358963856, -8.64810818633413], number: '11' },
+    { id: 'n1', position: [40.64284731231383, -8.648360316678158], number: '12' },
+    { id: 'n1', position: [40.642703172532975, -8.648684977669097], number: '13' },
+    //{ id: 'n2', latitude: 40.642850, longitude: -8.648600, number: '2' },
+    // Additional numbers...
+  ];
 
   useEffect(() => {
     const fetchAndUpdateData = async () => {
       try {
         const response = await fetch('/data');
         const data = await response.json();
-        const obuEntries = [];
-        const rsuEntries = [];
-
-        Object.keys(data).forEach(key => {
-          if (key.startsWith('OBU')) { // Assuming OBU data keys start with 'OBU'
-            obuEntries.push({
-              id: key,
-              ...data[key]
-            });
-          } else { // Assuming RSU data keys do not start with 'OBU'
-            rsuEntries.push({
-              id: key,
-              ...data[key]
-            });
-          }
-        });
-
-        setObuData(obuEntries);
-        setRsuData(rsuEntries);
+        const carEntries = Object.keys(data).map(key => ({
+          id: key,
+          ...data[key]
+        }));
+        setCarData(carEntries);
       } catch (error) {
         console.error('Failed to fetch data:', error);
       }
@@ -55,28 +64,48 @@ function MapComponent() {
 
   const carIcon = new L.Icon({
     iconUrl: require('./icons/car.png'),
-    iconSize: new L.Point(50, 50),
+    iconSize: new L.Point(20, 20),
     className: 'car-icon'
   });
 
+  const trafficLightIcon = new L.Icon({
+    iconUrl: require('./icons/traffic-light.png'),
+    iconSize: new L.Point(20, 20),
+    className: 'traffic-light-icon'
+  });
+
+  const createNumberIcon = (number) => {
+    return new L.divIcon({
+      html: <div style="background-color: rgba(255, 255, 255, 0.8); border-radius: 50%; border: 2px solid black; width: 24px; height: 24px; display: flex; align-items: center; justify-content: center; color: black; font-weight: bold;">${number}</div>,
+      className: '',  // No additional classes needed
+      iconSize: [15, 1],
+      iconAnchor: [12, 12]  // Centers the icon
+    });
+  };
+
   return (
     <div className='container'>
-      {/* <Toggle defaultChecked={showHighwayInfo} onChange={() => setShowHighwayInfo(!showHighwayInfo)} /> */}
-      <MapContainer center={[40.641754, -8.652605]} zoom={17.5} style={{ height: "100%", width: "100%"}} scrollWheelZoom={false}>
-        <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
-        {obuData.map((obu, index) => (
-          <Marker key={index} position={[obu.latitude, obu.longitude]} icon={carIcon}>
+      <MapContainer center={[40.64264002592938, -8.648293544440508]} zoom={17.5} style={{ height: "100%", width: "100%"}} scrollWheelZoom={false}>
+        <TileLayer url="https://api.mapbox.com/styles/v1/jp-amaral/cl758vsmy000714o2rx0w0779/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoianAtYW1hcmFsIiwiYSI6ImNsNzU4c3g1MzExMHozbm1hdWlvbnRrbmoifQ.SpZQvOQyQCwhNZluPGPXQg" />
+        {carData.map(car => (
+          <Marker key={car.id} position={[car.latitude, car.longitude]} icon={carIcon}>
             <Popup>
-              Speed: {obu.speed} km/h
-              ID: {obu.id}
+              Speed: {car.speed} km/h
+              ID: {car.id}
             </Popup>
           </Marker>
         ))}
-        {rsuData.map((rsu, index) => (
-          <Marker key={index} position={[rsu.latitude, rsu.longitude]} icon={carIcon}>
+        {trafficLights.map(light => (
+          <Marker key={light.id} position={[light.latitude, light.longitude]} icon={trafficLightIcon}>
             <Popup>
-              Speed: {rsu.speed} km/h
-              ID: {rsu.id}
+              Traffic Light ID: {light.id}
+            </Popup>
+          </Marker>
+        ))}
+        {numberPositions.map(pos => (
+          <Marker key={pos.id} position={pos.position} icon={createNumberIcon(pos.number)}>
+            <Popup>
+              Number: {pos.number}
             </Popup>
           </Marker>
         ))}
